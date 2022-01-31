871e57b55e42f017ef6d8574ec5750a47b55e827
Almog Attal <almogattal@gmail.com>
2022-01-31 14:04:54 +0200

    added new article route

--- routes/articleSystem/newArticle.js
+++ routes/articleSystem/newArticle.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const ArticleModel = require('../../model/articleModel')
+const UserModel = require('../../model/userModel')
 const ArticleValidation = require('../../validation/articleValidation')
 
 router.post('/', async (req,res)=>{
     try {
         const value = await ArticleValidation.articleSchema.validateAsync(req.body, { abortEarly: false })
         console.log(value)
+        console.log(req.tokenData)
+        const findCreator = await UserModel.findUserbyId(req.tokenData.id)
+        console.log(findCreator)
+        const newArticle = await ArticleModel.createArticle({
+            title: value.title,
+            articleBody: value.articleBody,
+            createdBy: findCreator.id,
+        })
+        res.json({ status: 200, article: newArticle})
     } catch (err) {
-        res.status(400).json({ status: 400, err:err })
+        res.status(400).json({ status: 400, err: err })
     }
 })
 
